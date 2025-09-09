d73a483f4ffa25ffe82301244231f952e31143b3
LijnxLaptop <lynx20030311@gmail.com>
2025-09-09 16:31:25 +0700

    task done

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import os
 import numpy
 import matplotlib
-import imageio
+import imageio as iio
 
+def get_images(dir_path):
+    
+    images = []
+    for entry in os.listdir(dir_path):
+        full_path = os.path.join(dir_path, entry)
+        images.append(iio.imread(full_path))
+    return images
+
+
+images = get_images("circle-split")
+iio.mimwrite("animation.gif", images, duration=0.2)
